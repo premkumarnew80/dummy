a7a7dfdd17f0ff94da7a4c7c5918b78974ea78c4
Premkumar Manipillai <premkumar.manipillai@htcindia.local>
2021-12-14 05:07:40 +0530

    index file added in src folder

---
+++ src/main/java/spring/htc/git/HtcGitApplication.java
@@ -0,0 +1,13 @@
+package spring.htc.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtcGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtcGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/htc/git/HtcGitApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.htc.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtcGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
